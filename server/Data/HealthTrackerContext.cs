e4b5d4c39920c38c49d044c003df83776f93c3d2
Momo3945 <moohoo03@gmail.com>
2024-07-28 12:04:11 +0200

    added stored procedure

--- server/Data/HealthTrackerContext.cs
+++ server/Data/HealthTrackerContext.cs
@@ -40,5 +40,55 @@ namespace HealthTrackerApi.Data
                 entity.Property(e => e.Longitude);
             });
         }
+
+        public async Task CreateDeleteOldDiagnosesProcedureAsync()
+        {
+            string createProcedureSql = @"
+                CREATE PROCEDURE DeleteOldDiagnoses
+                AS
+                BEGIN
+                    -- HIV-AIDS: retain for 10 years
+                    DELETE FROM Diagnoses WHERE DiagnosisName = 'HIV-AIDS' AND DateOfDiagnosis < DATEADD(YEAR, -10, GETDATE());
+
+                    -- Tuberculosis: retain for 6 months
+                    DELETE FROM Diagnoses WHERE DiagnosisName = 'Tuberculosis' AND DateOfDiagnosis < DATEADD(MONTH, -6, GETDATE());
+
+                    -- Malaria: retain for 6 days
+                    DELETE FROM Diagnoses WHERE DiagnosisName = 'Malaria' AND DateOfDiagnosis < DATEADD(DAY, -6, GETDATE());
+
+                    -- COVID-19: retain for 2 weeks
+                    DELETE FROM Diagnoses WHERE DiagnosisName = 'COVID-19' AND DateOfDiagnosis < DATEADD(DAY, -14, GETDATE());
+
+                    -- Cholera: retain for 1 week
+                    DELETE FROM Diagnoses WHERE DiagnosisName = 'Cholera' AND DateOfDiagnosis < DATEADD(DAY, -7, GETDATE());
+
+                    -- Ebola: retain for 3 weeks
+                    DELETE FROM Diagnoses WHERE DiagnosisName = 'Ebola' AND DateOfDiagnosis < DATEADD(DAY, -21, GETDATE());
+
+                    -- Zika: retain for 1 week
+                    DELETE FROM Diagnoses WHERE DiagnosisName = 'Zika' AND DateOfDiagnosis < DATEADD(DAY, -7, GETDATE());
+
+                    -- Measles, Mumps, Rubella: retain for 2 weeks
+                    DELETE FROM Diagnoses WHERE DiagnosisName IN ('Measles', 'Mumps', 'Rubella') AND DateOfDiagnosis < DATEADD(DAY, -14, GETDATE());
+
+                    -- Hepatitis A: retain for 3 months
+                    DELETE FROM Diagnoses WHERE DiagnosisName = 'Hepatitis A' AND DateOfDiagnosis < DATEADD(MONTH, -3, GETDATE());
+
+                    -- Hepatitis B and C: retain for 10 years
+                    DELETE FROM Diagnoses WHERE DiagnosisName IN ('Hepatitis B', 'Hepatitis C') AND DateOfDiagnosis < DATEADD(YEAR, -10, GETDATE());
+
+                    -- Dengue, Yellow Fever: retain for 2 weeks
+                    DELETE FROM Diagnoses WHERE DiagnosisName IN ('Dengue', 'Yellow Fever') AND DateOfDiagnosis < DATEADD(DAY, -14, GETDATE());
+
+                    -- Polio: retain for 6 months
+                    DELETE FROM Diagnoses WHERE DiagnosisName = 'Polio' AND DateOfDiagnosis < DATEADD(MONTH, -6, GETDATE());
+
+                    -- Default case for 'Other' diagnoses: retain for 1 month
+                    DELETE FROM Diagnoses WHERE DiagnosisName = 'Other' AND DateOfDiagnosis < DATEADD(MONTH, -1, GETDATE());
+                END;
+            ";
+
+            await Database.ExecuteSqlRawAsync(createProcedureSql);
+        }
     }
 }
